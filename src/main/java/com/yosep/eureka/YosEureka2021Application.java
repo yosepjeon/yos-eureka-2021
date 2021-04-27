a7d21e11655a3068060124bac17ce6052d8da4e4
yosepjeon <enekelx1@naver.com>
2021-04-27 17:24:22 +0900

    Initial commit

---
+++ src/main/java/com/yosep/eureka/YosEureka2021Application.java
@@ -0,0 +1,20 @@
+package com.yosep.eureka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
+
+@EnableEurekaServer
+@SpringBootApplication
+@RefreshScope
+public class YosEureka2021Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(YosEureka2021Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/yosep/eureka/YosEureka2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yosep.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YosEureka2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
